2918bca5a9df7fb9b3f7b70903462bf2ca44a16c
ponakilan <ponakilan25@gmail.com>
2023-04-04 11:38:20 +0530

    Issue - null input app crashing fixed

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.tiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Double
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
@@ -22,8 +24,11 @@ class MainActivity : AppCompatActivity() {
      * Calculate the tip amount and update the view with the tip amount
      */
     private fun calculateTip() {
-        // Getting the input amount
-        val cost = binding.costOfService.text.toString().toDouble()
+        val cost = binding.costOfService.text;
+        if (cost.isEmpty()) {
+            Snackbar.make(binding.scrollView, "Enter some amount", Snackbar.LENGTH_SHORT).show();
+            return;
+        }
 
         // Getting the selected tip percentage
         val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
@@ -33,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Calculating the tip and rounding it off if the round off switch is toggled
-        var tipAmount = tipPercentage * cost
+        var tipAmount = tipPercentage * cost.toString().toDouble()
         if (binding.roundUpSwitch.isChecked) { tipAmount = kotlin.math.ceil(tipAmount) }
 
         // Formatting the tip amount based on local currency and updating the view with the final tip amount
